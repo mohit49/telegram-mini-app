6bb0494b17f7dbdfe21c7025d27995fac3f9b1fd
mohit49 <mohitanim@gmail.com>
2024-12-13 18:13:32 +0530

    changes

--- pages/friend.tsx
+++ pages/friend.tsx
@@ -96,9 +96,9 @@ function Friend({ tetegram, user }: FriendProps) {
     <>
       <Image src={shareHand} alt="share image" />
       <div className="text-center px-3 pb-4 m-auto w-full">
-        <div className="rounded-[10px] p-4 bg-[#f0f0f0] w-[90%] mt-10 ml-auto mr-auto mb-3 border border-[#cccccc] flex flex-row justify-between">
+        <div className="rounded-[10px] py-2 bg-[#f0f0f0] px-4 w-[90%] items-start mt-10 ml-auto mr-auto mb-3 border border-[#cccccc] flex flex-row justify-between">
           <p className="font-bold">You earned</p>
-          <p className="font-bold flex gap-2">{data?.credit}  <Image className="h-[35px] w-[35px] object-cover " src={DimondIcon} alt="Dimond" /></p>
+          <p className="font-bold flex items-center gap-2">{data?.credit}  <Image className="h-[40px] w-[40px] object-cover " src={DimondIcon} alt="Dimond" /></p>
         </div>
 
         {!refrals?.referrerDetails || refrals.referrerDetails.length === 0 ? (
