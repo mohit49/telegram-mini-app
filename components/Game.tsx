3c5a7538d5e3e6418d4c8c1294865b56316e6891
mohit49 <mohitanim@gmail.com>
2024-12-15 00:11:29 +0530

    gamse splash screen

--- components/Game.tsx
+++ components/Game.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import FlappyBird from "./FlappyBird";
 import Image from "next/image";
-import birdTemplet from "@/public/Template.png";
+import birdTemplet from "@/public/template.png";
 import animgif from "@/public/start_animation.gif";
 import Footer from "./Footer";
 import Background from "./Background";
